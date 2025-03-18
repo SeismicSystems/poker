50628a7d9e56235eeab4e66d8ecb0c2674a0714b
Ameya Deshmukh <ad@seismic.systems>
2025-03-18 06:39:55 +0530

    basic contract and tests

--- packages/contracts/script/Poker.s.sol
+++ packages/contracts/script/Poker.s.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+contract Poker {
+    suint256 private counter;
+    address private admin;
+
+    constructor() {
+        counter = suint256(0);
+        admin = msg.sender;
+    }
+
+    function increment(suint256 amount) public {
+        counter += amount;
+    }
+
+    function viewCounter() public view onlyAdmin returns (uint256) {
+        return uint256(counter);
+    }
+
+    modifier onlyAdmin() {
+        if (msg.sender != admin) {
+            revert("Only admin can call this function");
+        }
+        _;
+    }
+}
